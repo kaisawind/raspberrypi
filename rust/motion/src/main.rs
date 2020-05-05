f021b1420817963803377d0a463a578fd416c18a
kaisawind <wind.kaisa@gmail.com>
2020-05-05 19:35:42 +0800

    fixed motion

--- rust/motion/src/main.rs
+++ rust/motion/src/main.rs
@@ -4,15 +4,18 @@ use std::time::Duration;
 
 use rppal::gpio::Gpio;
 
-
 fn main() -> Result<(), Box<dyn Error>> {
     // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
     let motion_pin: u8 = 23;
     // Retrieve the GPIO pin and configure it as an output.
-    let mut pin = Gpio::new()?.get(motion_pin)?.into_output();
+    let pin = Gpio::new()?.get(motion_pin)?.into_input();
 
     loop {
-        pin.toggle();
+        if pin.is_low() {
+            println!("Nothing moves ...");
+        } else {
+            println!("Motion detected ...");
+        }
         thread::sleep(Duration::from_millis(500));
     }
 }
